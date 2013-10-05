1ed3d91f0771363b085f65ec00b9ac176d62bf0e
vickeeyz <vickeeyz@laptop.(none)>
2013-10-05 20:47:49 +0530

    modified unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,24 @@
-worker_processes 3
+worker_processes 1
 timeout 30
 preload_app true
+
+# rails_root  = `pwd`.gsub("\n", "")
+# @dir = "#{rails_root}/"
+working_directory = "/home/vickeeyz/deploy/passty/current/"
+
+# listen on both a Unix domain socket and a TCP port,
+# we use a shorter backlog for quicker failover when busy
+listen "/home/vickeeyz/deploy/passty/tmp/sockets/unicorn.sock", :backlog => 64
+
+# listen 8080, :tcp_nopush => true
+# feel free to point this anywhere accessible on the filesystem
+pid "#{@dir}tmp/pids/unicorn.pid"
+
+# By default, the Unicorn logger will write to stderr.
+# Additionally, ome applications/frameworks log to stderr or stdout,
+# so prevent them from going to /dev/null when daemonized here:
+stderr_path "#{@dir}log/unicorn.stderr.log"
+stdout_path "#{@dir}log/unicorn.stdout.log"
  
 before_fork do |server, worker|
   # Replace with MongoDB or whatever
